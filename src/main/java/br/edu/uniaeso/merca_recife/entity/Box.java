39ec92f9d0e41929c2ea8289e88d516ea55cc3e2
EsleyFerreira <esley.fs25@gmail.com>
2025-05-08 01:01:34 -0300

    Created Box entity and support for images

---
+++ src/main/java/br/edu/uniaeso/merca_recife/entity/Box.java
@@ -0,0 +1,41 @@
+package br.edu.uniaeso.merca_recife.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_box")
+public class Box {
+    @Id
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "market_id", referencedColumnName = "id", nullable = false, updatable = false)
+    private Market market;
+
+    @Column(columnDefinition = "BINARY LARGE OBJECT")
+    private byte[] image;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Market getMarket() {
+        return market;
+    }
+
+    public void setMarket(Market market) {
+        this.market = market;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+}
