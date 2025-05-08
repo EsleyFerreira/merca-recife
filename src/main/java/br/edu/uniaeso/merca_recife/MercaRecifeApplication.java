057f0833b783355a63dd0ab4b0f5b2637791ead6
EsleyFerreira <esley.fs25@gmail.com>
2025-05-08 01:38:42 -0300

    Initialized Spring Boot project with standard structure

---
+++ src/main/java/br/edu/uniaeso/merca_recife/MercaRecifeApplication.java
@@ -0,0 +1,12 @@
+package br.edu.uniaeso.merca_recife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercaRecifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercaRecifeApplication.class, args);
+	}
+}
---
+++ src/test/java/br/edu/uniaeso/merca_recife/MercaRecifeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.uniaeso.merca_recife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercaRecifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
