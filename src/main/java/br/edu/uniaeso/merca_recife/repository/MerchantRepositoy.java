f6932ba9a3a854aba8ec1cd1a3eae33757f3c727
EsleyFerreira <esley.fs25@gmail.com>
2025-05-14 20:56:26 -0300

    create merchant repository interface for jpa

--- src/main/java/br/edu/uniaeso/merca_recife/repository/MerchantRepositoy.java
+++ src/main/java/br/edu/uniaeso/merca_recife/repository/MerchantRepositoy.java
@@ -2,6 +2,8 @@ package br.edu.uniaeso.merca_recife.repository;
 
 import br.edu.uniaeso.merca_recife.entity.Merchant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MerchantRepositoy extends JpaRepository<Merchant, Long> {
 }
