eed1f2c54acedcd2d0160ad9f2e9486ff4be972a
EsleyFerreira <esley.fs25@gmail.com>
2025-05-14 21:30:24 -0300

    Implement REST endpoints for Merchant management in MerchantResource

--- src/main/java/br/edu/uniaeso/merca_recife/resource/MerchantResource.java
+++ src/main/java/br/edu/uniaeso/merca_recife/resource/MerchantResource.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/merchants")
-public class MerchantController {
+public class MerchantResource {
 
     @Autowired
     private MerchantService merchantService;
