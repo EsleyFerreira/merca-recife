c1c155fee6c681239f32b3a9d82e2b036f812997
EsleyFerreira <esley.fs25@gmail.com>
2025-05-29 01:43:13 -0300

    updating the import organization

--- src/main/java/br/edu/uniaeso/merca_recife/service/impl/MarketServiceImpl.java
+++ src/main/java/br/edu/uniaeso/merca_recife/service/impl/MarketServiceImpl.java
@@ -6,7 +6,6 @@ import br.edu.uniaeso.merca_recife.service.MarketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 
--- src/main/java/br/edu/uniaeso/merca_recife/service/impl/MerchantServiceImpl.java
+++ src/main/java/br/edu/uniaeso/merca_recife/service/impl/MerchantServiceImpl.java
@@ -5,7 +5,6 @@ import br.edu.uniaeso.merca_recife.repository.MerchantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 
