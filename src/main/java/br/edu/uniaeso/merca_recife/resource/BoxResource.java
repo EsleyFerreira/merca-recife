9975f945e05613f4a0d5d0712350b30f3b905ddc
EsleyFerreira <esley.fs25@gmail.com>
2025-05-29 01:36:21 -0300

    Implement Box CRUD endpoints, setting Market on creation

--- src/main/java/br/edu/uniaeso/merca_recife/resource/BoxResource.java
+++ src/main/java/br/edu/uniaeso/merca_recife/resource/BoxResource.java
@@ -2,12 +2,15 @@ package br.edu.uniaeso.merca_recife.resource;
 
 import br.edu.uniaeso.merca_recife.dto.BoxDTO;
 import br.edu.uniaeso.merca_recife.entity.Box;
+import br.edu.uniaeso.merca_recife.entity.Market;
 import br.edu.uniaeso.merca_recife.mapper.BoxMapper;
 import br.edu.uniaeso.merca_recife.service.BoxService;
+import br.edu.uniaeso.merca_recife.service.MarketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,19 +22,28 @@ public class BoxResource {
     @Autowired
     private BoxService boxService;
 
+    @Autowired
+    private MarketService marketService;
+
     @PostMapping
     public ResponseEntity<BoxDTO> createBox(@RequestBody BoxDTO boxDTO) {
+        if (boxDTO.getMarketId() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "marketId não pode ser nulo");
+        }
+
+        Market market = marketService.findById(boxDTO.getMarketId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Market não encontrado com id: " + boxDTO.getMarketId()));
 
         Box box = BoxMapper.toEntity(boxDTO);
-        Box savedBox = boxService.save(box);
+        box.setMarket(market);
 
+        Box savedBox = boxService.save(box);
         return new ResponseEntity<>(BoxMapper.toDTO(savedBox), HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<List<BoxDTO>> getAllBoxes() {
         List<Box> boxes = boxService.findAll();
-
         List<BoxDTO> boxDTOs = boxes.stream()
                 .map(BoxMapper::toDTO)
                 .collect(Collectors.toList());
@@ -48,12 +60,14 @@ public class BoxResource {
 
     @PutMapping("/{id}")
     public ResponseEntity<BoxDTO> updateBox(@PathVariable Long id, @RequestBody BoxDTO boxDTO) {
-        try {
-            Box boxDetails = BoxMapper.toEntity(boxDTO);
-            Box updatedBox = boxService.update(id, boxDetails);
 
+        Box boxDetails = BoxMapper.toEntity(boxDTO);
+
+        try {
+            Box updatedBox = boxService.update(id, boxDetails);
             return ResponseEntity.ok(BoxMapper.toDTO(updatedBox));
         } catch (RuntimeException e) {
+
             return ResponseEntity.notFound().build();
         }
     }
